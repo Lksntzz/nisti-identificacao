76930a1667150dd81e1d06ab98560a65ae165f77
Lksntzz <lucasmanga95@gmail.com>
2026-08-18 09:41:09 -0300

    feat: route protected product finish updates

--- src/edge-router.js
+++ src/edge-router.js
@@ -96,7 +96,7 @@ function isProtectedApi(pathname) {
 }
 
 async function loadApp() {
-  const module = await import('./storage-metrics-router.js');
+  const module = await import('./product-finish-router.js');
   return module.default;
 }
 
