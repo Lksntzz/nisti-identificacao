608901e37db1be266f509bac6e42996d4bf27481
Lksntzz <lucasmanga95@gmail.com>
2026-08-18 10:48:52 -0300

    perf: enable embedding-first identification path

--- src/performance-router.js
+++ src/performance-router.js
@@ -1,5 +1,5 @@
 import app from './compact-admin-router.js';
-import { fastIdentify } from './fast-identify-adaptive.js';
+import { fastIdentify } from './fast-identify-hybrid.js';
 import { parseSku } from './sku.js';
 
 function responseWithHeaders(response, extra = {}) {
