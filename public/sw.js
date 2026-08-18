537d3077420f81cfb44b8f0ef529e5ecc0ae33cb
Lksntzz <lucasmanga95@gmail.com>
2026-08-18 15:24:34 -0300

    chore: refresh PWA cache for full catalog recognition

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nisti-identificacao-v12';
+const CACHE_NAME = 'nisti-identificacao-v13';
 const SHELL_KEY = '/__nisti_shell__';
 
 self.addEventListener('install', () => self.skipWaiting());
