9eb0d9a7a4c60cbb24c19b367593483eddd6d244
Lucas Santos <lucasmanga95@gmail.com>
2026-09-26 10:14:54 -0300

    fix(commerce): correct cutover SQL dollar quoting

--- supabase/cutover/20260926_rollback_live_cutover.sql
+++ supabase/cutover/20260926_rollback_live_cutover.sql
@@ -75,7 +75,7 @@ insert into public.commerce_reconciliation_candidates select * from commerce_cut
 insert into public.commerce_update_items select * from commerce_cutover_backup_20260926.commerce_update_items;
 insert into public.commerce_update_checks select * from commerce_cutover_backup_20260926.commerce_update_checks;
 
-do $
+do $$
 declare
   r record;
 begin
@@ -87,9 +87,9 @@ begin
     execute r.definition;
   end loop;
 end
-$;
+$$;
 
-do $
+do $$
 declare
   suffix text;
   live_table text;
